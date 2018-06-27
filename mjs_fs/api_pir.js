4ca1f351b95ad00f846ffff6b67473417a83851f
Tomasz Kopacki <tomasz@kopacki.eu>
2018-06-27 11:27:59 +0200

    .

--- mjs_fs/api_pir.js
+++ mjs_fs/api_pir.js
@@ -5,8 +5,9 @@ load('api_timer.js');
 
 function init() {
     GPIO.set_mode(Cfg.get('pir.pin'), GPIO.MODE_INPUT);
-    Timer.set(1000 * 5, Timer.REPEAT, function () {
+    Timer.set(1000 * 1, Timer.REPEAT, function () {
         MQTT.pub(Cfg.get('pir.topic'), JSON.stringify(GPIO.read(Cfg.get('pir.pin'))), 1, false);
+        print(JSON.stringify(GPIO.read(Cfg.get('pir.pin'))));
     }, null);
 }
 
