b9a8b568b28c7c63786fcee397b67842faf749ac
Tomasz Kopacki <tomasz@kopacki.eu>
2018-06-27 21:15:55 +0200

    .

--- mjs_fs/api_pir.js
+++ mjs_fs/api_pir.js
@@ -10,11 +10,16 @@ function init() {
     Timer.set(1000 * 1, Timer.REPEAT, function () {
         let current = GPIO.read(Cfg.get('pir.pin'));
         if(current !== previous) {
+            print("State update to", current);
             previous = current;
             MQTT.pub(Cfg.get('pir.topic'), JSON.stringify(current), 1, false);
+        } else {
+            print("No change on state observed");
         }
     }, null);
 }
 
 MQTT.pub(Cfg.get('pir.topic'), "Hello !", 1, false);
+print("Init");
 init();
+print("Init done");
